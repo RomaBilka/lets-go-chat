c454bdefde02807b7dca1f84e2da86bfdc2522a6
Roman Biliak <roman_biliak@epam.com>
2021-10-27 16:53:34 +0300

    go files

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/RomaBiliak/lets-go-chat/pkg/hasher"
+)
+
+func main() {
+	password, confirm := "test", "test"
+	hashPassword, err := hasher.HashPassword(password)
+	if err == nil {
+		fmt.Println(hasher.CheckPasswordHash(confirm, hashPassword))
+	}
+	fmt.Println(err)
+}
