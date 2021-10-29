46c822203257baba81ef53ffb51587f28b7f0dda
Roman Biliak <roman_biliak@epam.com>
2021-10-29 13:14:58 +0300

    fix

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -4,13 +4,17 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// HashPassword returns the bcrypt hash of the password and error
+// HashPassword returns the bcrypt hash of the password
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 5)
-	return string(bytes), err
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
 
-// CheckPasswordHash compares a password and bcrypt hashed, returns a Boolean value
+// CheckPasswordHash compares a password and bcrypt hashed
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
